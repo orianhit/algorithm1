6f1fb3f7c6383d5aa5801de84a9d1d9a77e041ea
orianhit <85432004+orianhit@users.noreply.github.com>
2021-12-14 07:34:00 +0200

    Update independent_sub_series_max.py

--- independent_sub_series_max.py
+++ independent_sub_series_max.py
@@ -1,26 +1,42 @@
-INPUT_A = [10, 1, 2, 5, 37, 100]
+# INPUT_A = [10, 1, 2, 5, 37, 100]
 
-MAYBE_RESULT = {
-    True: [],
-    False: []
-}
+INPUT_A = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-append_to_chain = True
-MAX_SUM_IN_IDX = [0 for i in range(len(INPUT_A) + 1)]
 
-for idx, arr_var in enumerate(INPUT_A):
-    if idx == 0 and arr_var > 0:
-        MAX_SUM_IN_IDX[1] = arr_var
-        MAYBE_RESULT[append_to_chain].append(arr_var)
-    else:
-        should_add = arr_var + MAX_SUM_IN_IDX[idx - 1] > MAX_SUM_IN_IDX[idx]
-        if should_add:
-            MAX_SUM_IN_IDX[idx + 1] = arr_var + MAX_SUM_IN_IDX[idx - 1]
-            MAYBE_RESULT[append_to_chain].append(arr_var)
+def get_max_ind_sum(arr, debug=False, print_idx=False):
+    maybe_result = {
+        True: [],
+        False: []
+    }
+
+    append_to_chain = True
+    max_sum_per_idx = [0 for i in range(len(arr) + 1)]
+
+    for idx, arr_var in enumerate(arr):
+        to_append = idx if print_idx else arr_var
+        if idx == 0 and arr_var > 0:
+            max_sum_per_idx[1] = arr_var
+            maybe_result[append_to_chain].append(to_append)
         else:
-            MAX_SUM_IN_IDX[idx + 1] = MAX_SUM_IN_IDX[idx]
-            MAYBE_RESULT[append_to_chain] = MAYBE_RESULT[not append_to_chain].copy()
-    append_to_chain = not append_to_chain
+            should_add = arr_var + max_sum_per_idx[idx - 1] > max_sum_per_idx[idx]
+            if should_add:
+                max_sum_per_idx[idx + 1] = arr_var + max_sum_per_idx[idx - 1]
+                maybe_result[append_to_chain].append(to_append)
+            else:
+                max_sum_per_idx[idx + 1] = max_sum_per_idx[idx]
+                # this line can be cost O(n) but can be implemented as 2way_linked_list
+                maybe_result[append_to_chain] = maybe_result[not append_to_chain].copy()
+        if debug:
+            print("=====================================")
+            print(f"first list: {maybe_result[True]}")
+            print(f"second list: {maybe_result[False]}")
+        append_to_chain = not append_to_chain
 
-print(MAYBE_RESULT[not append_to_chain])
-print(sum(MAYBE_RESULT[not append_to_chain]))
+    print(maybe_result[not append_to_chain])
+    output_sum = sum(maybe_result[not append_to_chain])
+    print(output_sum)
+    return output_sum
+
+
+if __name__ == '__main__':
+    get_max_ind_sum(INPUT_A, True, True)
