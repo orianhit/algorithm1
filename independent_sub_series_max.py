61ffc09166ad69f3b1581cd7995faf9571556d7d
orianhit <85432004+orianhit@users.noreply.github.com>
2021-12-14 21:36:50 +0200

    Update independent_sub_series_max.py

--- independent_sub_series_max.py
+++ independent_sub_series_max.py
@@ -1,42 +1,58 @@
+from treelib import Node, Tree
+
 # INPUT_A = [10, 1, 2, 5, 37, 100]
+
 
 INPUT_A = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 
-def get_max_ind_sum(arr, debug=False, print_idx=False):
+def get_max_ind_sum(arr, debug=False):
+    tree = Tree()
+    tree.create_node("root", "root")
+
     maybe_result = {
-        True: [],
-        False: []
+        'A': None,
+        'B': None
     }
 
-    append_to_chain = True
+    append_to_chain = 'A'
     max_sum_per_idx = [0 for i in range(len(arr) + 1)]
 
     for idx, arr_var in enumerate(arr):
-        to_append = idx if print_idx else arr_var
         if idx == 0 and arr_var > 0:
             max_sum_per_idx[1] = arr_var
-            maybe_result[append_to_chain].append(to_append)
+            tree.create_node(arr_var, idx, parent="root")
+            maybe_result[append_to_chain] = idx
         else:
             should_add = arr_var + max_sum_per_idx[idx - 1] > max_sum_per_idx[idx]
             if should_add:
                 max_sum_per_idx[idx + 1] = arr_var + max_sum_per_idx[idx - 1]
-                maybe_result[append_to_chain].append(to_append)
+                parent = "root" if maybe_result[append_to_chain] == None else maybe_result[append_to_chain]
+                tree.create_node(arr_var, idx, parent=parent)
+                maybe_result[append_to_chain] = idx
             else:
                 max_sum_per_idx[idx + 1] = max_sum_per_idx[idx]
                 # this line can be cost O(n) but can be implemented as 2way_linked_list
-                maybe_result[append_to_chain] = maybe_result[not append_to_chain].copy()
+                maybe_result[append_to_chain] = maybe_result['B' if append_to_chain == 'A' else 'A']
         if debug:
             print("=====================================")
-            print(f"first list: {maybe_result[True]}")
-            print(f"second list: {maybe_result[False]}")
-        append_to_chain = not append_to_chain
+            tree.show()
+        append_to_chain = 'B' if append_to_chain == 'A' else 'A'
 
-    print(maybe_result[not append_to_chain])
-    output_sum = sum(maybe_result[not append_to_chain])
-    print(output_sum)
+    output_sum = print_output(append_to_chain, maybe_result, tree)
+    return output_sum
+
+
+def print_output(append_to_chain, maybe_result, tree):
+    parent = tree.get_node(maybe_result['B' if append_to_chain == 'A' else 'A'])
+    print(f"Last tree node (representing reversed root of solution) is: {parent}")
+    output_sum = 0
+    while parent and parent.identifier != 'root':
+        output_sum += int(parent.tag)
+        parent = tree.parent(parent.identifier)
+    print(f"Max sum is {output_sum}")
     return output_sum
 
 
 if __name__ == '__main__':
-    get_max_ind_sum(INPUT_A, True, True)
+    get_max_ind_sum(INPUT_A, True)
